5dedc32d1d4f3cb471ddc370e55f8288ed1d2fca
sarita devkar <devkarsarita012@gmail.com>
2022-08-01 03:24:06 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,26 @@
+window.onscroll = function() {myFunction()};
+
+var navbar = document.getElementById("navbar");
+var sticky = navbar.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
+
+
+
+const basicAutocomplete = document.querySelector('#search-autocomplete');
+const data = ['Reliance industries', 'Nifty Bank index', 'SBI small cap fund', 'kotak nifty etf', 'kitkat'];
+const dataFilter = (value) => {
+  return data.filter((item) => {
+    return item.toLowerCase().startsWith(value.toLowerCase());
+  });
+};
+
+new mdb.Autocomplete(basicAutocomplete, {
+  filter: dataFilter
+});
