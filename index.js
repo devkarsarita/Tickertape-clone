3e9a0a5563ba0e7f2df1d9e529f9b74d6dd17d3e
sarita devkar <devkarsarita012@gmail.com>
2022-08-17 17:18:32 +0530

    update files

--- index.js
+++ index.js
@@ -1,26 +1,12 @@
-window.onscroll = function() {myFunction()};
-
-var navbar = document.getElementById("navbar");
-var sticky = navbar.offsetTop;
-
-function myFunction() {
-  if (window.pageYOffset >= sticky) {
-    navbar.classList.add("sticky")
-  } else {
-    navbar.classList.remove("sticky");
-  }
-}
 
 
 
-const basicAutocomplete = document.querySelector('#search-autocomplete');
-const data = ['Reliance industries', 'Nifty Bank index', 'SBI small cap fund', 'kotak nifty etf', 'kitkat'];
-const dataFilter = (value) => {
-  return data.filter((item) => {
-    return item.toLowerCase().startsWith(value.toLowerCase());
-  });
-};
 
-new mdb.Autocomplete(basicAutocomplete, {
-  filter: dataFilter
-});
+
+
+const toggleButton = document.getElementsByClassName('toggle-button')[0]
+const navbarLinks = document.getElementsByClassName('navbar-links')[0]
+
+toggleButton.addEventListener('click', () => {
+  navbarLinks.classList.toggle('active')
+})
